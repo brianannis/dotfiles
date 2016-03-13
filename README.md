a00ecdb945e7c15e3aa1faecdd42827cedab70e2
Brian <brian@Nova.nyc.rr.com>
2016-03-12 21:41:48 -0500

    Unify launch appearance
    Added TODO and zsh profile

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dotfiles
 OS X Bootstrapping
+
+### How to get started ###
+- Clone repo
+- ```shell
+chmod a+x launch.sh
+```
+- ```shell
+./launch.sh
+```
--- launch.sh
+++ launch.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 set -e
-echo "Preflight check:"
 
-if ! command -v git > /dev/null; then
-   echo "Installing Xcode Tools"
+echo "       __      __  _____ __         ";
+echo "  ____/ /___  / /_/ __(_) /__  _____";
+echo " / __  / __ \/ __/ /_/ / / _ \/ ___/";
+echo "/ /_/ / /_/ / /_/ __/ / /  __(__  ) ";
+echo "\__,_/\____/\__/_/ /_/_/\___/____/  ";
+echo "                                    ";
+
+echo "Preflight check:"
+echo ""
+if ! [ -x /usr/bin/git ]; then
+   echo "Installing xcode tools"
    xcode-select --install
 else
-   echo -e "\x1B[01;92m Xcode Tools installed \x1B[0m"
+   echo -e "\033[0;32m xcode tools installed \x1B[0m"
 fi
 
-if ! command -v brew > /dev/null; then
+if ! [ -x /usr/local/bin/brew ]; then
    echo "Installing brew"
    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-   echo -e "\x1B[01;92m Brew installed \x1B[0m"
+   echo -e "\033[0;32m brew installed \x1B[0m"
 fi
 
 if ! [ -x /usr/local/bin/python ]; then
    echo "Installing homebrew python"
    brew install python
 else
-  echo -e "\x1B[01;92m python installed \x1B[0m"
+  echo -e "\033[0;32m python installed \x1B[0m"
 fi
 
 if ! [ -x /usr/local/bin/python ]; then
    echo "Upgrading pip"
    pip install --upgrade pip
 else
-  echo -e "\x1B[01;92m pip upgraded \x1B[0m"
+  echo -e "\033[0;32m pip upgraded \x1B[0m"
 fi
 
 if ! [ -x /usr/local/bin/ansible ]; then
    echo "Installing ansible"
    pip install ansible
 else
-  echo -e "\x1B[01;92m ansible installed \x1B[0m"
+  echo -e "\033[0;32m ansible installed \x1B[0m"
 fi
-
-echo "Initilize launch playbook"
+echo ""
+echo -e "Initilize playbook:\n"
 ansible-playbook launch.yml
