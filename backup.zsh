6b1d68a80fb40cf0f75fcd0a31268e3c76b3bb8c
Brian Annis <6808268+brianannis@users.noreply.github.com>
2023-07-22 13:52:26 -0400

    backup

--- backup.zsh
+++ backup.zsh
@@ -1,10 +1,17 @@
 #!/bin/zsh
+# ./backup.zsh any-password
 
 # create encrypted store
-printf '%s' $1 | hdiutil create -encryption AES-128 -stdinpass -size 1g -fs APFS -volname transfer $HOME/transfer.dmg
+printf '%s' $1 | hdiutil create -encryption AES-128 -stdinpass -size 10g -fs APFS -volname transfer $HOME/transfer.dmg
 
 # mount disk
 printf '%s' $1 | hdiutil attach -stdinpass $HOME/transfer.dmg
 
+# copy dotfiles
+cp -r $HOME/{.aws,.kube,.ssh,.docker/config.json,.vault*,.zprofile,.zshrc,Documents,Desktop,Downloads} /Volumes/transfer
+
+# copy ccMenu config
+defaults export -app CCMenu /Volumes/transfer/ccmenu.plist
+
 # unmount
-hdiutil unmount /dev/disk5s1 
+# hdiutil unmount /dev/disk5s1 
---
+++ restore.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# ./restore.zsh any-password
+
+# mount disk
+printf '%s' $1 | hdiutil attach -stdinpass $HOME/transfer.dmg
+
+# copy dotfiles
+cp -r /Volumes/transfer/{.aws,.kube,.ssh,.docker/config.json,.vault*,.zprofile,.zshrc,Documents,Desktop,Downloads} $HOME/
+
+# write ccMenu config
+defaults import net.sourceforge.cruisecontrol.CCMenu ccmenu.plist
+
+# unmount
+# hdiutil unmount /dev/disk5s1 
