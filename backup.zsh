cc584e0dbcbd629928d7b085c8ddf211d2521f1a
Brian Annis <6808268+brianannis@users.noreply.github.com>
2023-07-22 14:42:14 -0400

    docs

--- backup.zsh
+++ backup.zsh
@@ -2,15 +2,19 @@
 # ./backup.zsh any-password
 
 # create encrypted store
+echo -e "creating encrypted volume"
 printf '%s' $1 | hdiutil create -encryption AES-128 -stdinpass -size 10g -fs APFS -volname transfer $HOME/transfer.dmg
 
 # mount disk
+echo -e "mounting encrypted volume"
 printf '%s' $1 | hdiutil attach -stdinpass $HOME/transfer.dmg
 
 # copy dotfiles
+echo -e "copying files to encrypted volume"
 cp -r $HOME/{.aws,.kube,.ssh,.docker/config.json,.vault*,.zprofile,.zshrc,Documents,Desktop,Downloads} /Volumes/transfer
 
 # copy ccMenu config
+echo -e "copy ccMenu config"
 defaults export -app CCMenu /Volumes/transfer/ccmenu.plist
 
 # unmount
--- restore.zsh
+++ restore.zsh
@@ -2,13 +2,17 @@
 # ./restore.zsh any-password
 
 # mount disk
+echo -e "attempting to mount disk"
 printf '%s' $1 | hdiutil attach -stdinpass $HOME/transfer.dmg
 
 # copy dotfiles
+echo -e "copying files to $HOME"
 cp -r /Volumes/transfer/{.aws,.kube,.ssh,.docker/config.json,.vault*,.zprofile,.zshrc,Documents,Desktop,Downloads} $HOME/
 
 # write ccMenu config
-defaults import net.sourceforge.cruisecontrol.CCMenu ccmenu.plist
+echo -e "writing ccMenu config"
+defaults import net.sourceforge.cruisecontrol.CCMenu /Volumes/transfer/ccmenu.plist
 
 # unmount
+echo -e "detach transfer volume"
 hdiutil detach /Volumes/transfer
