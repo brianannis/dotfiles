e030b726569ed0899595ae7a7d6329a839375c46
Brian Annis <6808268+brianannis@users.noreply.github.com>
2025-10-04 11:09:11 -0400

    Update bootstrap.zsh

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -77,7 +77,7 @@ echo -e "Initialize playbook:\n"
 if ! command -v ansible-playbook &> /dev/null; then
     echo -e "\033[0;31m Error: ansible-playbook not found in PATH \033[0m"
     echo "You may need to restart your shell or run: source ~/.zprofile"
-    exit 1
+    source ~/.zprofile
 fi
 
 ansible-playbook launch.yml
